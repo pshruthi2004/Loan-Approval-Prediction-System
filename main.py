569f9b0778b56c3206638d383095fdbe98ac8200
pshruthi2004 <pshruthi2004@gmail.com>
2026-04-29 13:25:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Loan Approval Prediction System Project")
+
+print("""
+Steps to Run:
+
+1. Install requirements:
+   pip install -r requirements.txt
+
+2. Train the model:
+   python train.py
+
+3. Run Flask app:
+   python app.py
+""")
